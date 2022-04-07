baaca16f9ef897d262e8dbb302baa18144af1800
Seonghun <dev.seonghun@gmail.com>
2022-04-08 03:09:33 +0900

    add email verified feature

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes');
-var usersRouter = require('./routes/users');
-var nftRouter = require('./routes/nft');
-var authRouter = require('./routes/auth');
+let authCache = {};
+global.authCache = authCache;
+
+var indexRouter = require("./routes");
+var usersRouter = require("./routes/users");
+var nftRouter = require("./routes/nft");
+var authRouter = require("./routes/auth");
 var app = express();
 
 // Default Setting
 const port = process.env.PORT || 3000;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Router Setting
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/nft', nftRouter);
-app.use('/auth', authRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/nft", nftRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -38,11 +41,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
