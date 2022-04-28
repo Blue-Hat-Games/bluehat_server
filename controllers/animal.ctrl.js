708e576c6814adf6e08ad53c3552e1033c417273
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-04-28 20:03:46 +0900

    Feat : Add random animal and save to db

--- controllers/animal.ctrl.js
+++ controllers/animal.ctrl.js
@@ -1,62 +1,35 @@
+const models = require("../models");
+
 exports.getUserAnimal = function (req, res, next) {
-  // DB에서 유저가 가진 모든 동물 조회후 반환
-  let userId = req.params.id;
-  let animals = ["Rabbit"];
-  return res.status(200).send(animals);
+	// DB에서 유저가 가진 모든 동물 조회후 반환
+	let userId = req.params.id;
+	let animals = ["Rabbit"];
+	return res.status(200).send(animals);
 };
 
-exports.getNewAnimal = function (req, res, next) {
-  // 새로운 동물 생성
-  const animals = [
-    "ArcticFox",
-    "Ox",
-    "Penguin",
-    "PolarBear",
-    "Reindeer",
-    "SeaLion",
-    "SnowOwl",
-    "SnowWeasel",
-    "Walrus",
-    "Buffalo",
-    "Chick",
-    "Cow",
-    "Donkey",
-    "Duck",
-    "Hen",
-    "Pig",
-    "Rooster",
-    "Sheep",
-    "Crow",
-    "Eagle",
-    "Fox",
-    "Hog",
-    "Hornbill",
-    "Owl",
-    "Raccoon",
-    "Snake",
-    "Wolf",
-    "Cat",
-    "Dog",
-    "Dove",
-    "Goldfish",
-    "Mice",
-    "Parrot",
-    "Pigeon",
-    "Rabbit",
-    "Tortoise",
-    "Cheetah",
-    "Elephant",
-    "Flamingo",
-    "Gazelle",
-    "Hippo",
-    "Hyena",
-    "Ostrich",
-    "Rhino",
-    "Zebra",
-    "Husky",
-  ];
-  let animalPickIndex = Math.floor(Math.random() * animals.length);
-  let animal = animals[animalPickIndex];
-  // DB에 새로운 동물 추가
-  return res.status(201).send(animal);
+exports.getNewAnimal = async function (req, res, next) {
+	try {
+		// 새로운 동물 생성
+		const allAnimalLength = await models.animal.count();
+		let animalPickIndex = Math.floor(Math.random() * allAnimalLength + 1);
+		let animal = await models.animal.findOne({ where: { id: animalPickIndex } });
+
+		await models.animal_possession
+			.create({
+				nft_hash: "0",
+				color: null,
+				name: "testAnimal",
+				tier: 1,
+				user_id: req.body.userId,
+				animal_id: animal.id,
+			})
+			.then((user) => {
+				console.log(user);
+			});
+
+		return res.status(201).send(animal);
+	} catch (e) {
+		console.log(e);
+		return res.status(500).send("Internal Server Error");
+	}
 };
