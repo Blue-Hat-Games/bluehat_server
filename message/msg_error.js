9e63487996c09491c37f8e73b22f3f7d28d09eff
chanyeop <153893@naver.com>
2022-04-30 14:06:13 +0900

    Chore: detach message templates

---
+++ message/msg_error.js
@@ -0,0 +1,15 @@
+exports.internalServerError = {
+    msg: "Internal Server Error"
+};
+
+exports.emailNotVerified = {
+    msg: "email not verified"
+}
+
+exports.duplicateInfo = {
+    msg: "dup email or wallet_address or username"
+}
+
+exports.fail = {
+    msg: "fail"
+}
---
+++ message/msg_info.js
@@ -0,0 +1,3 @@
+exports.success = {
+    msg: "success"
+}
