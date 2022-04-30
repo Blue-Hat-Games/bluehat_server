1edff0bee9d45b05810c88bb0777d71fba18442b
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-04-30 18:51:30 +0900

    Feat : change getAnimal api return

--- controllers/animal.ctrl.js
+++ controllers/animal.ctrl.js
@@ -5,7 +5,22 @@ exports.getUserAnimal = async function (req, res, next) {
 	try {
 		// DB에서 유저가 가진 모든 동물 조회후 반환
 		let userId = req.userId;
-		let userAnimal = await models.animal_possession.findAll({ where: { user_id: userId } })
+		let userAnimal = await models.animal_possession.findAll({ where: { user_id: userId }, include: [{
+			model: models.animal,
+			attributes: ['type'],
+		  },{
+			model: models.head_item,
+			attributes: ['filename']
+		  },{
+			model: models.body_item,
+			attributes: ['filename']
+		  },{
+			model: models.foot_item,
+			attributes: ['filename']
+		  },{
+			model: models.pattern,
+			attributes: ['filename']
+		  }], attributes: [ 'name', 'tier', 'color'],raw: true});
 		return res.status(200).send(userAnimal);
 	} catch (e) {
 		console.log(e);
@@ -28,6 +43,10 @@ exports.getNewAnimal = async function (req, res, next) {
 				tier: 1,
 				user_id: req.userId,
 				animal_id: animal.id,
+				head_item_id: 1,
+				body_item_id :1,
+				foot_item_id: 1,
+				pattern_id: 1,
 			})
 			.then((user) => {
 				console.log(user);
