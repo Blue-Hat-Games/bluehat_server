763028c80ddb1d179154fb5694b5c16211f86a6f
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-04-30 14:49:58 +0900

    Fix: Username allow null

--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@ module.exports = function (sequelize, DataTypes) {
             },
             username: {
                 type: DataTypes.STRING,
-                allowNull: false,
+                allowNull: true,
                 unique: true
             },
             coin: {
