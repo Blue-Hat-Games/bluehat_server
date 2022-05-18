51efcb6b11d23e0b15044b6be61bac92ea38c8aa
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-05-18 20:56:05 +0900

    Feat : get user nft animal

--- controllers/nft.ctrl.js
+++ controllers/nft.ctrl.js
@@ -4,6 +4,7 @@ const errorMsg = require("../message/msg_error");
 const infoMsg = require("../message/msg_info");
 const verifyUtils = require("../utils/verify");
 const { Op } = require("sequelize");
+const logger = require("../config/logger");
 
 exports.mergeAnimal = async function (req, res, next) {
 	const { animalId1, animalId2, color } = req.body;
@@ -16,8 +17,8 @@ exports.mergeAnimal = async function (req, res, next) {
 			where: { [Op.or]: [{ id: animalId1 }, { id: animalId2 }] },
 		});
 
-        let user_wallet = await models.user.findOne({ where : { id : req.userId }});
-        console.log(user_wallet.wallet_address);
+		let user_wallet = await models.user.findOne({ where: { id: req.userId } });
+		console.log(user_wallet.wallet_address);
 
 		function randomVal() {
 			return Math.round(Math.random());
@@ -34,9 +35,9 @@ exports.mergeAnimal = async function (req, res, next) {
 			pattern_id: animals[randomVal()].pattern_id,
 		};
 
-        let nftMintResult = await nftUtils.getNft(title='mergeAnimal', symbol=verifyUtils.encryptValtest(JSON.stringify(new_animal)), toAddr=user_wallet.wallet_address);
-        console.log(nftMintResult);
-        new_animal['nft_hash'] = nftMintResult.transactionHash;
+		let nftMintResult = await nftUtils.getNft(title = 'mergeAnimal', symbol = verifyUtils.encryptValtest(JSON.stringify(new_animal)), toAddr = user_wallet.wallet_address);
+		console.log(nftMintResult);
+		new_animal['nft_hash'] = nftMintResult.transactionHash;
 
 		let new_animals = await models.animal_possession.create(new_animal);
 
@@ -53,3 +54,92 @@ exports.mergeAnimal = async function (req, res, next) {
 		return res.status(500).send(errorMsg.internalServerError);
 	}
 };
+
+
+exports.getUserNftAnimal = async function (req, res, next) {
+	logger.info(`${req.method} ${req.url}`);
+
+	// Setting Default
+	if (!req.query.page) {
+		req.query.page = 1;
+	}
+	if (!req.query.limit) {
+		req.query.limit = 20;
+	}
+
+	const offset = (parseInt(req.query.page) - 1) * parseInt(req.query.limit);
+	const limit = parseInt(req.query.limit);
+
+	orderInfo = {
+		Recently: ["createdAt", "DESC"],
+		Oldest: ["createdAt", "ASC"],
+		PriceLow: ["price", "ASC"],
+		PriceHigh: ["price", "DESC"],
+	};
+
+	if (!req.query.order) {
+		req.query.order = orderInfo["Recently"];
+	} else {
+		if (!orderInfo[req.query.order]) {
+			req.query.order = orderInfo["Recently"];
+		} else {
+			req.query.order = orderInfo[req.query.order];
+		}
+	}
+	const order = req.query.order;
+
+	try {
+		let allAnimal = await models.animal_possession.findAll({
+			where : { user_id: req.userId, nft_hash : {[Op.ne] : null} },
+			limit: limit,
+			offset: offset,
+			order: [order],
+			raw: true,
+		});
+		return res.status(200).send(allAnimal);
+	} catch (e) {
+		logger.error(e);
+		return res.status(500).send(errorMsg.internalServerError);
+	}
+
+
+}
+
+exports.getUserNftAnimalCount = async function (req, res, next) {
+	logger.info(`${req.method} ${req.url}`);
+	try {
+		let count = await models.animal_possession.count({ where: { user_id: req.userId, nft_hash : { [Op.ne] : null} } });
+		result = {
+			"status": "success",
+			"data": {
+				"totalCount": count
+			}
+		}
+		return res.status(200).send(result);
+	} catch (e) {
+		logger.error(e);
+		return  res.status(500).send(errorMsg.internalServerError);
+	}
+}
+
+exports.getUserNftAnimalById = async function (req, res, next) {
+	logger.info(`${req.method} ${req.url}`);
+	if (!req.params.id) {
+		return res.status(400).send(errorMsg.needParameter);
+	}
+	try {
+		let possessionInfo = await models.animal_possession.findOne({
+			where: { id: req.params.id },
+			attributes: ["id", "nft_hash", "color", "name", "tier", "animal_id", "head_item_id", "body_item_id", "foot_item_id",
+			"foot_item_id", "pattern_id", "createdAt", "updatedAt"]
+		});
+		if (!possessionInfo) {
+			return res.status(400).send({"status" : "fail", "msg": "No animal existed"});
+		}
+		return res.status(200).send(possessionInfo);
+		} catch (e) {
+			logger.error(e);
+			return res.status(500).send(errorMsg.internalServerError);
+		}
+	
+}
