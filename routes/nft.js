9d6e8e5a2a902a511eb4447273da0ed86bf32b76
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-08-02 15:58:42 +0900

    Fix : nft router test-nft to mint

--- routes/nft.js
+++ routes/nft.js
@@ -5,12 +5,12 @@ const { verifyToken } = require('../middlewares/verify');
 const { uploadFile } = require('../middlewares/file');
 
 
-router.post("", verifyToken,nftCtrl.mergeAnimal);
+router.post("", verifyToken, nftCtrl.mergeAnimal);
 router.get("/user/animal", verifyToken, nftCtrl.getUserNftAnimal);
 router.get("/user/animal/count", verifyToken, nftCtrl.getUserNftAnimalCount);
 router.get("/user/animal/:id", verifyToken, nftCtrl.getUserNftAnimalById);
 router.get("/metadata/:id", nftCtrl.getMetaData);
 router.post("/upload-ipfs", uploadFile, nftCtrl.uploadIpfs)
-router.post("/test-nft", express.urlencoded({ extended: false }), express.json(),uploadFile, nftCtrl.makeNFT)
+router.post("/mint", express.urlencoded({ extended: false }), express.json(), uploadFile, nftCtrl.makeNFT)
 
 module.exports = router;
