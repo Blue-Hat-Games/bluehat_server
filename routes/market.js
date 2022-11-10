b09b1a426faf3678d7e23e4b8389b3f18d85cc9e
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-11-10 18:43:14 +0900

    HotFix: Delete unuse route

--- routes/market.js
+++ routes/market.js
@@ -4,12 +4,9 @@ const marketCtrl = require("../controllers/market.ctrl");
 const { verifyToken } = require('../middlewares/verify');
 
 router.get("/list", marketCtrl.getAllMarketAnimal);
-router.post("/sell", verifyToken, marketCtrl.sellAnimaltoMarket);
 router.get("/counts", marketCtrl.getMarketAnimalCounts);
 router.get('/detail', marketCtrl.getMarketAnimalDetail);
+router.post("/sell", verifyToken, marketCtrl.sellAnimaltoMarket);
 router.post("/buy", verifyToken, marketCtrl.buyAnimalfromMarket);
-router.post("/trade-nft", marketCtrl.testNftTrade);
-router.post("/test", marketCtrl.testKIP17Trade)
-router.post("/test2", marketCtrl.sendToBuyUser)
 
 module.exports = router;
