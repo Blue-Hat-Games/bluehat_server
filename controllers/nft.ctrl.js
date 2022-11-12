6f23e22e8fa0cbc79e36794cf1e1d830f0f60d94
chanyeop <153893@naver.com>
2022-11-12 11:23:52 +0900

    Feat: handling failure of sending http request to blockchain system

--- controllers/nft.ctrl.js
+++ controllers/nft.ctrl.js
@@ -99,21 +99,29 @@ exports.makeNFT = async function (req, res) {
 		3. save to Database
 	*/
 	logger.info(`${req.method} ${req.originalUrl}`);
-	try {
-		let { animal_id, wallet_address } = req.body;
-		let imgHash = await nftUtils.uploadIpfsImg(req.file);
-		let tokenURL = await nftUtils.uploadIpfsMeta(imgHash);
-		let nftMintResult = await nftUtils.getNft(title = 'Bluehat Animal', symbol = 'Bluehat', tokenURL, toAddr = wallet_address);
-		await models.nft.create({
-			token_id: nftMintResult['events']['Transfer']['returnValues']['tokenId'],
-			ipfs_addr: tokenURL,
-			contract_addr: nftMintResult['events']['Transfer']['address'],
-		}).then(async (newNft) => {
-			await models.animal_possession.update({ nft_id: newNft.id }, { where: { id: animal_id } });
-			return res.status(200).send(infoMsg.success);
-		});
-	} catch (e) {
-		logger.error(e);
+	var try_count = 0;
+	const max_try_count = 3; //블록체인 상에 http 요청이 정상적으로 전달되지 않은 경우 3번까지 시도
+	while (try_count < max_try_count) {
+		try {
+			let { animal_id, wallet_address } = req.body;
+			let imgHash = await nftUtils.uploadIpfsImg(req.file);
+			let tokenURL = await nftUtils.uploadIpfsMeta(imgHash);
+			let nftMintResult = await nftUtils.getNft(title = 'Bluehat Animal', symbol = 'Bluehat', tokenURL, toAddr = wallet_address);
+			await models.nft.create({
+				token_id: nftMintResult['events']['Transfer']['returnValues']['tokenId'],
+				ipfs_addr: tokenURL,
+				contract_addr: nftMintResult['events']['Transfer']['address'],
+			}).then(async (newNft) => {
+				await models.animal_possession.update({ nft_id: newNft.id }, { where: { id: animal_id } });
+				return res.status(200).send(infoMsg.success);
+			});
+		} catch (e) {
+			logger.error(e);
+		}
+		try_count++;
+	}
+	if (try_count == max_try_count) {
+		await models.animal_possession.update({ nft_id: -1 }, { where: { id: animal_id } });//nft_id가 -1인 경우는 블록체인에 등록되지 않은 NFT
 		return res.status(500).send(errorMsg.internalServerError);
 	}
 };
